d5f8112b83c80c83a4f008a144799684b477b70f
chuongle1 <chuong.le1@tiki.vn>
2025-04-21 16:14:30 +0700

    Debug: add sqli vul

---
+++ sqli.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"database/sql"
+	"net/http"
+)
+
+func DeleteHandler(db *sql.DB) func(w http.ResponseWriter, req *http.Request) {
+	return func(w http.ResponseWriter, req *http.Request) {
+		del := req.URL.Query().Get("del")
+		id := req.URL.Query().Get("Id")
+		if del == "del" {
+			// ruleid: tainted-sql-string
+			_, err = db.Exec("DELETE FROM table WHERE Id = " + id)
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+}
