68f6e804cf15a05644c4ffdf9c9ce5f61af82ebf
anhkhoa14592 <anhkhoa14592@gmail.com>
2026-01-27 12:40:20 +0700

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "test: handle logic EC""""""""""""

--- sqli_7.go
+++ sqli_7.go
@@ -11,7 +11,8 @@ func DeleteHandler(db *sql.DB) func(w http.ResponseWriter, req *http.Request) {
 		del := req.URL.Query().Get("del")
 		id := req.URL.Query().Get("Id")
 		if del == "del" {
-			// ruleid: tainted-sql-string test 6
+			// ruleid: tainted-sql-string test 7
+			
 			_, err = db.Exec("DELETE FROM table WHERE Id = " + id) // test total scanned targets and findings
 			if err != nil {
 				panic(err)
